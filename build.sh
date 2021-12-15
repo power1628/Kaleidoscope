5c62f2519a7cb4f3d6dd57a15aba98182dd49832
PinGao <pin.gao2008@gmail.com>
2021-12-15 17:16:59 +0800

    3

---
+++ build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+clang++ -g -O3 toy.cpp `llvm-config --cxxflags --ldflags --system-libs --libs core` -o toy
+
--- toy.cpp
+++ toy.cpp
@@ -1,3 +1,15 @@
+#include "llvm/ADT/APFloat.h"
+#include "llvm/ADT/STLExtras.h"
+#include "llvm/IR/BasicBlock.h"
+#include "llvm/IR/Constants.h"
+#include "llvm/IR/DerivedTypes.h"
+#include "llvm/IR/Function.h"
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Verifier.h"
+
 #include <cctype>
 #include <cstdio>
 #include <cstdlib>
@@ -7,7 +19,7 @@
 #include <utility>
 #include <vector>
 
-
+using namespace llvm;
 
 /// Lexer
 
@@ -94,6 +106,8 @@ namespace {
 class ExprAST {
 public:
   virtual ~ExprAST() {}
+
+  virtual Value* codegen() = 0;
 };
 
 class NumberExprAST: public ExprAST {
@@ -101,6 +115,8 @@ class NumberExprAST: public ExprAST {
 
 public:
   NumberExprAST(double Val): Val(Val) {}
+
+  Value* codegen() override;
 };
 
 class VariableExprAST : public ExprAST {
@@ -108,6 +124,8 @@ class VariableExprAST : public ExprAST {
 
 public:
   VariableExprAST(const std::string& Name) : Name(Name) {}
+
+  Value* codegen() override;
 };
 
 class BinaryExprAST: public ExprAST {
@@ -117,6 +135,8 @@ class BinaryExprAST: public ExprAST {
 public:
   BinaryExprAST(char Op, std::unique_ptr<ExprAST> LHS, std::unique_ptr<ExprAST> RHS)
     : Op(Op), LHS(std::move(LHS)), RHS(std::move(RHS)) {}
+
+  Value* codegen() override;
 };
 
 class CallExprAST: public ExprAST {
@@ -126,6 +146,8 @@ class CallExprAST: public ExprAST {
 public:
   CallExprAST(const std::string& Callee, std::vector<std::unique_ptr<ExprAST> > Args)
     : Callee(Callee), Args(std::move(Args)) {}
+
+  Value* codegen() override;
 };
 
 
@@ -140,6 +162,7 @@ public:
   PrototypeAST(const std::string& Name, std::vector<std::string> Args)
     : Name(Name), Args(std::move(Args)) {}
 
+  Function* codegen();
   const std::string& getName() const { return Name; }
 };
 
@@ -150,8 +173,10 @@ class FunctionAST {
 
 public:
   FunctionAST(std::unique_ptr<PrototypeAST> Proto,
-              std::unique_ptr<ExprAST> body)
+              std::unique_ptr<ExprAST> Body)
     : Proto(std::move(Proto)), Body(std::move(Body)) {}
+
+  Function* codegen();
 };
 
 } // end of anonymous namespace
@@ -357,6 +382,7 @@ static std::unique_ptr<FunctionAST> ParseDefinition() {
   }
 
   if (auto E = ParseExpression()) {
+    fprintf(stderr, "not fucked \n");
     return std::make_unique<FunctionAST>(std::move(Proto), std::move(E));
   }
   return nullptr;
@@ -380,10 +406,150 @@ static std::unique_ptr<PrototypeAST> ParseExtern() {
   return ParsePrototype();
 }
 
-//////////// Top level parsing
+
+////////////////////// Codegen
+static std::unique_ptr<LLVMContext> TheContext;
+static std::unique_ptr<Module> TheModule;
+static std::unique_ptr<IRBuilder<>> Builder;
+static std::map<std::string, Value*> NamedValues;
+
+Value* LogErrorV(const char* Str) {
+  LogError(Str);
+  return nullptr;
+}
+
+Value* NumberExprAST::codegen() {
+  return ConstantFP::get(*TheContext, APFloat(Val));
+}
+
+Value* VariableExprAST::codegen() {
+  // Look this variable up in the function
+  Value* V = NamedValues[Name];
+  if (!V) {
+    return LogErrorV("Unknown variable name");
+  }
+  return V;
+}
+
+Value* BinaryExprAST::codegen() {
+  Value* L = LHS->codegen();
+  Value *R = RHS->codegen();
+  if (!L || !R) {
+    return nullptr;
+  }
+
+  switch (Op) {
+    case '+' :
+      return Builder->CreateFAdd(L, R, "addtmp");
+    case '-' :
+      return Builder->CreateFSub(L, R, "subtmp");
+    case '*' :
+      return Builder->CreateFMul(L, R, "multmp");
+    case '<' :
+      L = Builder->CreateFCmpULT(L, R, "cmptmp");
+      // convert bool 0/1 to double 0.0 or 1.0
+      return Builder->CreateUIToFP(L, Type::getDoubleTy(*TheContext), "booltmp");
+    default:
+      return LogErrorV("invalid binary operator");
+  }
+}
+
+Value* CallExprAST::codegen() {
+  // Lookup the name in the global module table.
+  Function* CalleeF = TheModule->getFunction(Callee);
+  if (!CalleeF) {
+    return LogErrorV("Unknown function referenced");
+  }
+
+  // If argument mismatch error.
+  if (CalleeF->arg_size() != Args.size()) {
+    return LogErrorV("Incorrect # arguments passed");
+  }
+
+  std::vector<Value*> ArgsV;
+  for (unsigned i = 0, e = Args.size(); i != e; ++i) {
+    ArgsV.push_back(Args[i]->codegen());
+    if (!ArgsV.back()) {
+      return nullptr;
+    }
+  }
+  
+  return Builder->CreateCall(CalleeF, ArgsV, "calltmp");
+}
+
+
+Function* PrototypeAST::codegen() {
+  // Make the function type: double(double, double) etc.
+  std::vector<Type*> Doubles(Args.size(), Type::getDoubleTy(*TheContext));
+  FunctionType *FT = FunctionType::get(Type::getDoubleTy(*TheContext), Doubles, false);
+
+  Function* F = Function::Create(FT, Function::ExternalLinkage, Name, TheModule.get());
+
+  // Set names for all arguments
+  unsigned Idx = 0;
+  for (auto& Arg: F->args()) {
+    Arg.setName(Args[Idx++]);
+  }
+
+  return F;
+}
+
+
+Function* FunctionAST::codegen() {
+  // First, check for an existing function from a previous 'extern' decl.
+  Function* TheFunction = TheModule->getFunction(Proto->getName());
+
+  if (!TheFunction) {
+    TheFunction = Proto->codegen();
+  }
+
+  if (!TheFunction) {
+    return nullptr;
+  }
+
+  // Create a new basic block to start insertion into.
+  BasicBlock* BB = BasicBlock::Create(*TheContext, "entry", TheFunction);
+  Builder->SetInsertPoint(BB);
+
+  // Record the function arguments in the NamedValues map.
+  NamedValues.clear();
+  for (auto& Arg: TheFunction->args()) {
+    NamedValues[std::string(Arg.getName())] = &Arg;
+  }
+
+  if (Value* RetVal = Body->codegen()) {
+    // Finish off the function
+    Builder->CreateRet(RetVal);
+
+    // Validate the generated code, checking for consistency.
+    verifyFunction(*TheFunction);
+    return TheFunction;
+  }
+
+  // Error reading body, remove function
+  TheFunction->eraseFromParent();
+  return nullptr;
+}
+
+
+//////////// Top level parsing and JIT driver
+
+static void InitializeModule() {
+  // Open a new context and module.
+  TheContext = std::make_unique<LLVMContext>();
+  TheModule = std::make_unique<Module>("my cool jit", *TheContext);
+
+  // Create a new builder for the module
+  Builder = std::make_unique<IRBuilder<>>(*TheContext);
+}
+
 static void HandleDefinition() {
-  if (ParseDefinition()) {
-    fprintf(stderr, "Parsed a function definition. \n");
+  if (auto FnAST = ParseDefinition()) {
+    if (auto* FnIR = FnAST->codegen()) {
+      fprintf(stderr, "Read function definition:");
+      FnIR->print(errs());
+      fprintf(stderr, "\n");
+    }
   } else {
     // Skip token for error recovery
     getNextToken();
@@ -391,8 +557,12 @@ static void HandleDefinition() {
 }
 
 static void HandleExtern() {
-  if (ParseExtern()) {
-    fprintf(stderr, "Parsed an extern\n");
+  if (auto ProtoAST = ParseExtern()) {
+    if (auto* FnIR = ProtoAST->codegen()) {
+      fprintf(stderr, "Read extern: ");
+      FnIR->print(errs());
+      fprintf(stderr, "\n");
+    }
   } else {
     // Skip token for error recovery
     getNextToken();
@@ -401,8 +571,15 @@ static void HandleExtern() {
 
 static void HandleTopLevelExpression() {
   // Evaluate a top-level expression into an anonymous function.
-  if (ParseTopLevelExpr()) {
-    fprintf(stderr, "Parsed a top-level expr\n");
+  if (auto FnAST = ParseTopLevelExpr()) {
+    if (auto* FnIR = FnAST->codegen()) {
+      fprintf(stderr, "Read top-level expr: ");
+      FnIR->print(errs());
+      fprintf(stderr, "\n");
+
+      // Remove the anonymous expression.
+      FnIR->eraseFromParent();
+    }
   } else {
     getNextToken();
   }
@@ -446,8 +623,14 @@ int main() {
   fprintf(stderr, "ready>");
   getNextToken();
 
+  // Make the module, which holds all the code.
+  InitializeModule();
+
   // Run the main 'interpreter lop' now
   MainLoop();
+
+  // Print out all of the generated code
+  TheModule->print(errs(), nullptr);
 
   return 0;
 }
